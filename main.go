79e39b03de91f9b64dd38e3a4dfa907379ff0b4f
Jack Rickards <jackrickards@monzo.com>
2024-07-06 13:40:56 +0100

    it works on line ranges and will find the code

--- main.go
+++ main.go
@@ -10,6 +10,55 @@ import (
 	"strings"
 )
 
+// wrapRequestStruct wraps the found struct in a testing framework setup
+func wrapRequestStruct(fset *token.FileSet, node ast.Node, line int) (ast.Node, error) {
+	var structNode ast.Node
+	ast.Inspect(node, func(n ast.Node) bool {
+		if cl, ok := n.(*ast.CompositeLit); ok {
+			startPos := fset.Position(n.Pos())
+			endPos := fset.Position(n.End())
+			// Check if the given line is within the start and end lines of this CompositeLit
+			if startPos.Line <= line && endPos.Line >= line {
+				if checkRequestStruct(cl) {
+					responseType := strings.Replace(cl.Type.(*ast.SelectorExpr).Sel.Name, "Request", "Response", 1)
+					responseExpr := &ast.CompositeLit{
+						Type: &ast.SelectorExpr{
+							X:   cl.Type.(*ast.SelectorExpr).X,
+							Sel: ast.NewIdent(responseType),
+						},
+					}
+					callExpr := &ast.CallExpr{
+						Fun: &ast.SelectorExpr{
+							X:   ast.NewIdent("m"),
+							Sel: ast.NewIdent("ExpectRequest"),
+						},
+						Args: []ast.Expr{
+							&ast.CallExpr{
+								Fun: &ast.SelectorExpr{
+									X:   ast.NewIdent("test"),
+									Sel: ast.NewIdent("RequestEqualTo"),
+								},
+								Args: []ast.Expr{cl},
+							},
+						},
+					}
+					wrappedExpr := &ast.CallExpr{
+						Fun: &ast.SelectorExpr{
+							X:   callExpr,
+							Sel: ast.NewIdent("RespondWith"),
+						},
+						Args: []ast.Expr{responseExpr},
+					}
+					structNode = wrappedExpr
+					return false // Found and transformed the struct
+				}
+			}
+		}
+		return true
+	})
+	return structNode, nil
+}
+
 // extractRequestStructFromLineRange searches for a "Request" struct initialization that spans a given line number.
 func extractRequestStructFromLineRange(fset *token.FileSet, node ast.Node, line int) ast.Expr {
 	var structExpr ast.Expr
@@ -51,7 +100,7 @@ func main() {
 import "ledgerproto"
 
 func main() {
-    rsp, err := ledgerproto.CalculateBalanceRequest{
+    rsp, err := foo.BarRequest{
         BalanceName: ledgerproto.BalanceNameInterestPayable,
         AccountId:   pot.AccountId,
         LegalEntity: currencyLegalEntityMap[pot.Currency],
@@ -66,16 +115,19 @@ func main() {
 		panic(err)
 	}
 
-	// Specify the known line number that the struct spans
-	for lineNumber := 1; lineNumber <= 10; lineNumber++ {
-		requestStruct := extractRequestStructFromLineRange(fset, file, lineNumber)
-		if requestStruct != nil {
-			var buf bytes.Buffer
-			printer.Fprint(&buf, fset, requestStruct)
-			os.Stdout.Write(buf.Bytes())
-			break
-		} else {
-			println("No Request struct found spanning line", lineNumber)
-		}
+	// Wrap the struct found at a specific line within its span
+	lineNumber := 10 // Update as needed
+	wrappedNode, err := wrapRequestStruct(fset, file, lineNumber)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the modified code
+	if wrappedNode != nil {
+		var buf bytes.Buffer
+		printer.Fprint(&buf, fset, wrappedNode)
+		os.Stdout.Write(buf.Bytes())
+	} else {
+		println("No Request struct found or transformation failed")
 	}
 }
