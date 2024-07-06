edbef3ef4152d3333849cde83ec96c08d8a6f200
Jack Rickards <jackrickards@monzo.com>
2024-07-06 18:40:35 +0100

    flake lock & update go package hash

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
             # remember to bump this hash when your dependencies change.
             # vendorHash = pkgs.lib.fakeHash;
 
-            vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            vendorHash = "sha256-WKz4ItmoSirQ4QiB0aZO9cmzhNY7IcAEeJrsHYrP1rk=";
           };
         });
 
