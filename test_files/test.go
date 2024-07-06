33de8f971ff1798f9eb0e37e68c3d3fcdf04b579
Jack Rickards <jackrickards@monzo.com>
2024-07-06 18:22:06 +0100

    add another "test case"

--- test_files/test.go
+++ test_files/test.go
@@ -8,13 +8,19 @@ import (
 func main() {
 	fmt.Println("Starting request processing...")
 
-	// Example struct initialization that the script will find and wrap
 	rsp, err := ledgerproto.CalculateBalanceRequest{
 		BalanceName: ledgerproto.BalanceNameInterestPayable,
 		AccountId:   "123456789",
 		LegalEntity: "Monzo Bank Limited",
 		Currency:    "GBP",
 	}.Send(ctx).DecodeResponse()
+
+	ledgerproto.CalculateBalanceRequest{
+		BalanceName: ledgerproto.BalanceNameInterestPayable,
+		AccountId:   "123456789",
+		LegalEntity: "Monzo Bank Limited",
+		Currency:    "GBP",
+	}
 
 	fmt.Println("Response:", rsp)
 }
